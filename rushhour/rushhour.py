46fb9d563de4f8c68360f138c772497a5305adbe
20928Tijmen <144214560+20928Tijmen@users.noreply.github.com>
2024-01-10 16:37:11 +0100

    Het huidige spelbord met letters (bedoelt als vehicles) erop. Ondersteunt 6x6 en 9x9 borden.

--- rushhour/rushhour.py
+++ rushhour/rushhour.py
@@ -1,8 +1,15 @@
 from vehicles import Car, Truck, Redcar
 
 class Board():
+    """This class represents the gameboard"""
 
     def __init__(self, filename):
+        """
+        This will initialize the game board.
+
+        Pre: A valid filename has to be given.
+        Post: The board is initialized based on the given file.
+        """
 
         self.boardinfo = []
 
@@ -23,9 +30,16 @@ class Board():
 
         self.gameboard = []
 
-        for ro in range(6):
+        if '6x6' in filename:
+            loop = 6
+        elif '9x9' in filename:
+            loop = 9
+        elif '12x12' in filename:
+            loop = 12
+
+        for ro in range(loop):
             row = []
-            for til in range(6):
+            for til in range(loop):
                 row.append('_')
             self.gameboard.append(row)
 
@@ -43,14 +57,19 @@ class Board():
                     self.gameboard[ro + l][colu] = self.boardinfo[k][0]
 
     def showboard(self):
+        """
+        This will display the current state of the board.
+        Pre: A board needs to have been initialized.
+        Post: The current state of the game board is printed.
+        """
         for i in range(len(self.gameboard)):
             for j in range(len(self.gameboard[i])):
                 print(f" {self.gameboard[i][j]} ", end="")
             print("\n")
 
 if __name__ == "__main__":
-    validboards = ["Rushhour6x6_1.csv", "Rushhour6x6_2.csv", "Rushhour6x6_3.csv"]
-    print("Available boards:\n\nRushhour6x6_1.csv\nRushhour6x6_2.csv\nRushhour6x6_3.csv\n")
+    validboards = ["Rushhour6x6_1.csv", "Rushhour6x6_2.csv", "Rushhour6x6_3.csv",  "Rushhour9x9_4.csv", "Rushhour9x9_5.csv", "Rushhour9x9_6.csv"]
+    print("Available boards:\n\nRushhour6x6_1.csv\nRushhour6x6_2.csv\nRushhour6x6_3.csv\nRushhour9x9_4.csv\nRushhour9x9_5.csv\nRushhour9x9_6.csv\n")
     select = 'Selection'
     
     while select not in validboards:
