5d27b808669cef44e8c8b00daf7a197fd25e7fe9
20928Tijmen <144214560+20928Tijmen@users.noreply.github.com>
2024-01-11 17:18:30 +0100

    Startscherm toegevoegd (waarbij men kan kiezen voor een bord)

--- RushHourJoost/MainRush.py
+++ RushHourJoost/MainRush.py
@@ -2,8 +2,19 @@ from GameBoardClass import GameBoard
 
 
 if __name__ == '__main__':
-    game = GameBoard()
-    game.add_cars()
+
+    validboards = ["Rushhour6x6_1.csv", "Rushhour6x6_2.csv", "Rushhour6x6_3.csv",  "Rushhour9x9_4.csv", "Rushhour9x9_5.csv", "Rushhour9x9_6.csv"]
+    print("Available boards:\n\nRushhour6x6_1.csv\nRushhour6x6_2.csv\nRushhour6x6_3.csv\nRushhour9x9_4.csv\nRushhour9x9_5.csv\nRushhour9x9_6.csv\n")
+    
+    select = 'Selection'
+    
+    while select not in validboards:
+        select = input("Select board: ")
+    
+    print('\n')
+
+    game = GameBoard(select)
+    game.add_cars(select)
 
     board = game.get_board()
     fancy_board = game.get_board_for_player()
