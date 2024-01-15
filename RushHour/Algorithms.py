fccde757ef4fb8dbe16e13c089b5966e0cf59099
Juicedt <jostielol01@gmail.com>
2024-01-15 14:01:31 +0100

    algo1 first draft

--- RushHour/Algorithms.py
+++ RushHour/Algorithms.py
@@ -2,26 +2,30 @@ import random
 
 class Algorithm_1():
 
-    def __init__(self, game, letter, direction) -> None:
+    def __init__(self, game) -> None:
         """
         Eerste Algoritme: RANDOM
 
         Pre: Gameboard en History
         """
-
         self.game = game
-        self.letter = letter
-        self.direction = direction
+
+        self.cars = list(game._dictionary_of_cars.keys())
+
     
-    def make_random_move(letter, direction):
+    def make_random_move(self):
         """
         make a random move
         pre:
         post:
         """
         # choose random car
-        
+        random_car = random.choice(self.cars)
         # choose random move
+        random_direction = random.choice([1, -1])
+        
+        return random_car, random_direction
+
 
 class Algorithm_2():
 
