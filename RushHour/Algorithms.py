38a191bf5d48f317e8250fcdd82b74d4e97dbde6
T.Kooijman <T.Kooijman@sp-byods-145-109-53-216.wireless.uva.nl>
2024-01-15 12:08:44 +0100

    Creating .py file for Algorithms

---
+++ RushHour/Algorithms.py
@@ -0,0 +1,13 @@
+class Algorithm_1():
+    def __init__(self) -> None:
+        """
+        Eerste Algoritme: RANDOM
+
+        Pre: Gameboard en History
+        Post: ??
+        """
+
+        # Random sort!
+    
+    def make_random_move():
+        pass
