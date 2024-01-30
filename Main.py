768cf0d932a92807fb211aba44251ff53721bef6
T.Kooijman <T.Kooijman@sp-byods-145-109-7-108.wireless.uva.nl>
2024-01-30 14:32:34 +0100

    getting rid of results class

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-from Algorithms import DFS, BFS, Astar, RandomMove, RandomLegalMove, RandomLegalRepeatMove
-from Classes import GameBoard, GameFile, History
+from code.Algorithms import DFS, BFS, Astar, RandomMove, RandomLegalMove, RandomLegalRepeatMove
+from code.Classes import GameBoard, GameFile, History
 
 # pip3 install matplotlib numpy
     
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 import os, random
 
 import pygame
-import time 
 import timeit
 
 import csv
@@ -401,14 +400,20 @@ def compare_BFS_DFS_ASTAR(csv_data_file):
     Algorithm,Board,Compute Time,Solution Path Length,Visited States Count
 
     """
+
+    Algorithms = ['BFS', 'DFS', 'ASTAR']
+    Board = []
+    Compute Time = []
+    Solution Path Length = []
+    Visited States Count = []
+
+
+
     # Open the CSV file for reading
     with open(csv_data_file, mode='r') as file:
         reader = csv.reader(file)
 
-        Algorithms = ['BFS', 'DFS', 'ASTAR']
-        BFS_data = []
-        DFS_data = []
-        ASTAR_data = []
+
 
        # Read and print each row in the CSV file
         # to change the data to a simpler form i used chatgpt for help
@@ -514,6 +519,47 @@ def run_algorithms_on_6x6_boards():
         print("Running Astar:")
         astar_algorithm(board_path)
 
+def run_algorithms_on_9x9_boards():
+    board_paths = [
+        "data/Rushhour9x9_4.csv",
+        "data/Rushhour9x9_5.csv",
+        "data/Rushhour9x9_6.csv"
+    ]
+
+    for board_path in board_paths:
+        print(f"\nRunning algorithms on board: {board_path}")
+        
+        # BFS
+        print("Running BFS:")
+        breadth_first_search(board_path)
+
+        # DFS
+        print("Running DFS:")
+        depth_first_search(board_path)
+
+        # Astar
+        print("Running Astar:")
+        astar_algorithm(board_path)
+
+def run_algorithms_on_12x12_board():
+    board_paths = [
+        "data/Rushhour12x12_7.csv",
+    ]
+
+    for board_path in board_paths:
+        print(f"\nRunning algorithms on board: {board_path}")
+        
+        # BFS
+        print("Running BFS:")
+        breadth_first_search(board_path)
+
+        # DFS
+        print("Running DFS:")
+        depth_first_search(board_path)
+
+        # Astar
+        print("Running Astar:")
+        astar_algorithm(board_path)
 
 def main():
 
@@ -537,7 +583,13 @@ def main():
         elif mode == 'algo':
             manual_algo_comparisons()
         elif mode == 'auto':
-            run_algorithms_on_6x6_boards()
+            board_size = input("on which boards? 6, 9, or 12 ")
+            if board_size == 6:
+                run_algorithms_on_6x6_boards()
+            elif board_size == 9:
+                run_algorithms_on_9x9_boards()
+            elif board_size == 12:
+                run_algorithms_on_12x12_board()
         elif mode == 'p':
             csv_data_file = 'data/algoritmen_data.csv'
             compare_BFS_DFS_ASTAR(csv_data_file)
