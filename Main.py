b6fe93fb1f3f0033f6180fd0b42b0a964c02671d
Tijmen <tijmenjan15@hotmail.com>
2024-01-31 15:25:02 +0100

    Functies weggehaald

--- Main.py
+++ Main.py
@@ -12,73 +12,6 @@ import timeit
 
 import csv
 
-
-def load_board_opstellingen(path: str) -> list[str]:
-    '''
-    Zet alle gegeven bord csv's in een folder
-    geef van deze folder de path als input
-    krijg een mooie lijst me paths naar elke bord file terug
-
-    input: 
-    - path: string = het relative path voor de folder waarin alle csv files voor de borden in staan
-    
-    output:  
-    - return: list[string] = voor elke file in de aangewezen folder -> relatieve pad + filenaam
-    
-    '''
-
-    return [os.path.join(path, file) for file in os.listdir(path)]
-    
-
-def get_paths_of_size(file_paths: list[str], size: str):
-    '''
-    geef alle paths en gewenste size 
-    je krijgt een lijst met paths van die size , 
-    en een lijst met de nummers van die borden
-    !! PATH FORMAT MOET EINDIGEN OP *6x6_1.csv of *12x12_1.csv WEGENS INDEXING !!
-
-    input:
-    - file_paths: list[str] = de paths naar alle borden 
-    - size: str = de player input word als string gelezen
-
-    output:
-    - paths: list[str] = lijst van filepaths met de gevraagde size
-    - options: list[str] = lijst met de board nummers, want zijn meerdere van elke size
-    
-    '''
-
-    paths = []
-    options = []
-
-    for path in file_paths:
-        if path[-7] == size[0] or path[-8] == size[0]: 
-            paths.append(path)
-            options.append(path[-5])
-
-    return paths, options
-
-
-def get_path_of_number(game_number: str, path_options: list[str]) -> str:
-    '''
-    bij input '3' krijg je het het eerste bord eindigend op 3.csv terug
-    '''
-    for path in path_options:
-        if path[-5] == game_number: 
-            return path
-    
-
-def pick_board_manualy() -> str:
-    '''
-    deze functie start een gesprek met de speler in de terminal
-    zo kies je een spel uit
-    '''
-    paths_all = load_board_opstellingen('data')
-    board_size = input("What board size would you like?\nOptions: 6, 9, 12\nInput here: ")
-    path_options, board_options = get_paths_of_size(paths_all, board_size)
-    game_number = input(f"Boards with size {board_size}\nOptions: {', '.join(board_options)}\nInput here: ")
-    return get_path_of_number(game_number, path_options)
-
-
 def pick_board_random() -> str:
     '''
     spreekt voorzich denk ik
@@ -122,7 +55,7 @@ def available_algorithms():
 
     return algorithms_dictionary
 
-def visual():
+def visualize_random():
     """
     Main function to run the Rush Hour game.
     """
@@ -637,7 +570,7 @@ def main():
             mode = input("\nDo you want to run the game in the Visualize-random mode, Experiment mode, manual_algo_comparison,\
 OR automatical_algo_comparison OR print results from automatical_algo_comparison? (v/e/algo/auto/p) ").lower()
         if mode == 'v':
-            visual()
+            visualize_random()
         elif mode == 'e':
             experiment()
         elif mode == 'algo':
